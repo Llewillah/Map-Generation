bbc23aa8ec9f1f35021070c2b45e33e00c774085
Llewillah <thalliwell34@gmail.com>
2026-09-06 23:07:02 +0100

    Dungeon Generation

--- Assets/BinarySpacePartitioning.cs
+++ Assets/BinarySpacePartitioning.cs
@@ -17,24 +17,25 @@ public class Room
         height = y2 - y1;
     }
 
-    public void UpdateBounds(int x1, int y1, int x2, int y2) 
+    public void UpdateBounds()
     {
-        this.x1 = x1; this.y1 = y1;
-        this.x2 = x2; this.y2 = y2;
+        width = x2 - x1;
+        height = y2 - y1;
     }
 
     public bool isLeaf() 
     {
         return (lRoom == null) && (rRoom == null);
     }
-
 }
 
 public class BinarySpacePartitioning : MonoBehaviour
 {
-    public int minWidth, minHeight;
+    public int minWidth, minHeight, maxTrim;
     Grid grid;
     Room root;
+    public bool dungeon = false;
+    public bool viewPath = true;
 
     public void SetUp(Grid grid) 
     {
@@ -45,6 +46,15 @@ public class BinarySpacePartitioning : MonoBehaviour
     {
         root = new Room(0, 0, grid.width - 1, grid.height - 1);
         CutRectangle(root);
+
+        if (dungeon)
+        {
+            TrimRoom(root);
+        }
+        else 
+        {
+            AddBorder(root);
+        }
         DisplayRooms(root);
         DrawPaths(root);
     }
@@ -95,15 +105,58 @@ public class BinarySpacePartitioning : MonoBehaviour
         }
     }
 
+    void TrimRoom(Room room) 
+    {
+        if (room.isLeaf())
+        {
+            int xTrim = Random.Range(1, maxTrim + 1);
+            int yTrim = Random.Range(1, maxTrim + 1 - xTrim);
+
+            for (int i = 0; i < xTrim; i++)
+            {
+                int rand = Random.Range(0, 2);
+                if (rand == 0)
+                {
+                    room.x1++;
+                }
+                else
+                {
+                    room.x2--;
+                }
+            }
+
+            for (int i = 0; i < yTrim; i++)
+            {
+                int rand = Random.Range(0, 2);
+                if (rand == 0)
+                {
+                    room.y1++;
+                }
+                else
+                {
+                    room.y2--;
+                }
+            }
+
+            room.UpdateBounds();
+        }
+        else 
+        {
+            TrimRoom(room.lRoom);
+            TrimRoom(room.rRoom);
+        }
+    }
+
     void DisplayRooms(Room room) 
     {
         //check if at bottom of the tree
         if (room.isLeaf())
         {
+
             //draw the area of the room
-            for (int x = room.x1 + 1; x < room.x2; x++)
+            for (int x = room.x1; x < room.x2; x++)
             {
-                for (int y = room.y1 + 1; y < room.y2; y++)
+                for (int y = room.y1; y < room.y2; y++)
                 {
                     grid.GetTile(x, y).ChangeColour(Color.white);
                 }
@@ -131,20 +184,35 @@ public class BinarySpacePartitioning : MonoBehaviour
 
 
             //Draw a line between the rooms
+            Color pathCol = (viewPath) ? Color.red : Color.white;
 
             for (int x = x1; x < x2; x++) 
             {
-                grid.GetTile(x, y1).ChangeColour(Color.red);
+                grid.GetTile(x, y1).ChangeColour(pathCol);
             }
 
             for (int y = y1; y < y2; y++) 
             {
-                grid.GetTile(x1, y).ChangeColour(Color.red);
+                grid.GetTile(x1, y).ChangeColour(pathCol);
             }
             //Check the next layer of rooms
 
             DrawPaths(room.lRoom);
             DrawPaths(room.rRoom);
+        }
+    }
+
+    void AddBorder(Room room) 
+    {
+        if (room.isLeaf())
+        {
+            room.x1++;
+            room.y1++;
+        }
+        else 
+        { 
+            AddBorder(room.lRoom);
+            AddBorder(room.rRoom);
         }
     }
 }
